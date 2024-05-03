88d37d9655a4c196300b34fbfb67942d8d33680f
Oleksii Haivoronskyi <oleksiihayvoronskyi@gmail.com>
2024-05-03 19:58:47 +0300

    Implement model display functionality in stage 2.1

---
+++ stage2.1_show_model_main.py
@@ -0,0 +1,18 @@
+# Import functions: 'load_data()' and 'create_cnn_model()':
+import stage2_create_model
+
+
+# Load data
+def main():
+    # Load data
+    data, target = stage2_create_model.load_data()
+
+    # Create model
+    model = stage2_create_model.create_cnn_model(data.shape[1:])
+
+    # Visualize model
+    stage2_create_model.visualize_model(model)
+
+
+if __name__ == "__main__":
+    main()
